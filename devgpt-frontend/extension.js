e65f3ec3f3496ee3a34d2df102f859c562530933
Devashish Khatavkar <devashish0706@gmail.com>
2024-02-24 16:48:17 -0600

    Fixed starter code

--- devgpt-frontend/extension.js
+++ devgpt-frontend/extension.js
@@ -12,16 +12,16 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "devgpt" is now active!');
+	console.log('Congratulations, your extension "devgpt-frontend" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('devgpt.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('devgpt-frontend.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from devGPT!');
+		vscode.window.showInformationMessage('Bozoanan and Lesi from DevGPT!');
 	});
 
 	context.subscriptions.push(disposable);
