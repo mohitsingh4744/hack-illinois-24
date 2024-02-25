ef0b38bd592b48dde838681938ed57c39699b2f7
Devashish Khatavkar <devashish0706@gmail.com>
2024-02-24 22:03:33 -0600

    Google search feature - bugs

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ async function automateSearch() {
     const browser = await puppeteer.launch({headless: false});
     const pages = await browser.pages();
     const page = pages[0];
-    await page.setViewport({ width: 1288, height: 800 });
+    await page.setViewport({ width: 1035, height: 800 });
     await page.goto('https://google.com');
-    await page.waitForSelector('.glFyf');
-    await page.type('input.glFyf', 'cat');
-    await page.click('input.gNO89b');
+    await page.type('textarea#APjFqb.gLFyf', 'bumanan srirajan');
+    // await page.click('input.gNO89b');
 }
 
 main()
