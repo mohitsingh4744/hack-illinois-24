ea26512ae1acf46291634d852cd8799160d53b00
Devashish Khatavkar <devashish0706@gmail.com>
2024-02-24 23:07:40 -0600

    Unfinished google search

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ function main() {
 
 async function automateSearch(searchText) {
     const browser = await puppeteer.launch({headless: false});
+
     const pages = await browser.pages();
     const page = pages[0];
-    await page.setViewport({ width: 1288, height: 800 });
+
+    await page.setViewport({ width: 1035, height: 800, deviceScaleFactor: 1 });
     await page.goto(`https://www.google.com/search?q=${encodeURIComponent(searchText)}`);
-    console.log("Popup page is open. Close it manually to end.");
+    
+    console.log("Pop-up page is open. Close it manually to end.");
 }
 
 main()
