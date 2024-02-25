99de865ff21029066ccfcbf4105a0498af8fa4a6
Mohit Singh <mohitsingh4744@gmail.com>
2024-02-24 22:56:33 -0600

    devgpt buttons

--- devgpt-frontend/extension.js
+++ devgpt-frontend/extension.js
@@ -1,36 +1,79 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+function sendCodeToBackend(code) {
+  console.log('Sending code to backend:', code);
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
+  const backendEndpoint = 'https://example.com'; // Replace with your actual backend URL
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ code }),
+  };
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "devgpt-frontend" is now active!');
+  console.log('HTTP Request:', backendEndpoint, requestOptions);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('devgpt-frontend.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Bozoanan and Lesi from DevGPT!');
-	});
-
-	context.subscriptions.push(disposable);
+  fetch(backendEndpoint, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      console.log('Backend response:', data);
+    })
+    .catch(error => {
+      console.error('Error sending code to backend:', error);
+    });
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+function activate(context) {
+  let disposable = vscode.commands.registerCommand('extension.gptDebug', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const selectedText = editor.document.getText(editor.selection);
+      console.log("gptDebug: \n");
+      sendCodeToBackend(selectedText);
+    } else {
+      vscode.window.showErrorMessage('No active text editor');
+    }
+  });
+
+  // Register the command
+  context.subscriptions.push(disposable);
+  let disposable2 = vscode.commands.registerCommand('extension.gptExplain', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const selectedText = editor.document.getText(editor.selection);
+      console.log("gptExplain: \n");
+      sendCodeToBackend(selectedText);
+    } else {
+      vscode.window.showErrorMessage('No active text editor');
+    }
+  });
+
+  // Register the command
+  context.subscriptions.push(disposable2);
+
+  let disposable3 = vscode.commands.registerCommand('extension.gptGoogle', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const selectedText = editor.document.getText(editor.selection);
+      console.log("gptGoogle: \n");
+      sendCodeToBackend(selectedText);
+    } else {
+      vscode.window.showErrorMessage('No active text editor');
+    }
+  });
+
+  // Register the command
+  context.subscriptions.push(disposable3);
+
+  
+}
+
+function deactivate() {
+  console.log('Extension deactivated');
+}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate
+};
